00627e7e63e4f040532477d1f39277156c92b877
Aryn Harmon <achcello@gmail.com>
2018-04-28 11:41:10 -0500

    create api request, get response

--- app/src/main/java/cs125/mp7/vote/MainActivity.java
+++ app/src/main/java/cs125/mp7/vote/MainActivity.java
@@ -12,16 +12,13 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
 
 import org.json.JSONObject;
 
@@ -29,11 +26,9 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
-    private SectionsPageAdapter mSectionsPageAdapter;
-
-    private ViewPager mViewPager;
-
     private static String address = "none";
+
+    private static RequestQueue requestQueue;
 
     /**
      * Google Civic Information API key
@@ -47,13 +42,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestQueue = Volley.newRequestQueue(this);
+
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate: Starting.");
 
-        mSectionsPageAdapter = new SectionsPageAdapter(getSupportFragmentManager());
+        SectionsPageAdapter mSectionsPageAdapter = new SectionsPageAdapter(getSupportFragmentManager());
 
         // Set up the ViewPager with the sections adapter.
-        mViewPager = findViewById(R.id.container);
+        ViewPager mViewPager = findViewById(R.id.container);
         setupViewPager(mViewPager);
 
         TabLayout tabLayout = findViewById(R.id.tabs);
@@ -110,31 +108,42 @@ public class MainActivity extends AppCompatActivity {
         builder.show();
     }
 
-    public void makeInformation() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Candidate Information");
-
-        // Set up the input
-        final EditText input = new EditText(this);
-        // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
-        builder.setView(input);
-
-        // Set up the buttons
-        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                address = input.getText().toString();
-                Log.d(TAG, "onClick: address set to: " + address);
-                Toast.makeText(MainActivity.this, "Address Set!", Toast.LENGTH_SHORT).show();
-            }
-        });
-        builder.show();
-    }
-
-    public void startAPICall() {
+    /**
+     * Send the address to the API and attempt to get a response.
+     */
+    void startAPICall() {
         String url = "https://www.googleapis.com/civicinfo/v2/representatives?address="
                 + getAddress(address)
                 + "&key=" + apiKey;
+        final android.content.Context context = getApplicationContext();
+        final Toast toast = Toast.makeText(context,
+                "Retrieving data . . .",
+                Toast.LENGTH_SHORT);
+        toast.show();
+        try {
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                    Request.Method.GET,
+                    url,
+                    null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(final JSONObject response) {
+                            Log.d(TAG, response.toString());
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(final VolleyError error) {
+                    Log.w(TAG, error.toString());
+                }
+            });
+            requestQueue.add(jsonObjectRequest);
+        } catch (Exception e) {
+            Toast toast2 = Toast.makeText(context,
+                    "Failed.",
+                    Toast.LENGTH_SHORT);
+            toast2.show();
+            e.printStackTrace();
+        }
     }
 
     private String getAddress(String address) {
